e12006bed9a183251056594685cc2d5b1bc0e135
Luiz Carlos de Lima <82920625+Luiz-Carlos-de-Lima@users.noreply.github.com>
2023-08-19 22:19:55 -0300

    feat: inicializaçao do projeto e criação de pagina de splash com animaçao

---
+++ lib/src/features/splash/splash_page.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+class SplashPage extends StatefulWidget {
+  const SplashPage({super.key});
+
+  @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  var _scale = 10.0;
+  var _animationOpcaityLogo = 0.0;
+
+  double get _logAnimationWidget => 100 * _scale;
+  double get _logAnimationHeigth => 120 * _scale;
+
+  @override
+  void initState() {
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      setState(() {
+        _animationOpcaityLogo = 1.0;
+        _scale = 1.0;
+      });
+    });
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: DecoratedBox(
+        decoration: const BoxDecoration(
+          image: DecorationImage(image: AssetImage('assets/images/background_image_chair.jpg'), fit: BoxFit.cover, opacity: 0.2),
+        ),
+        child: AnimatedOpacity(
+          duration: const Duration(seconds: 3),
+          curve: Curves.easeIn,
+          opacity: _animationOpcaityLogo,
+          child: Center(
+            child: AnimatedContainer(
+              duration: const Duration(seconds: 3),
+              curve: Curves.linearToEaseOut,
+              width: _logAnimationWidget,
+              height: _logAnimationHeigth,
+              child: Image.asset(
+                'assets/images/imgLogo.png',
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
